6c9db2b03c8f761d1db4c5a5d774c2ce5278e339
Burak Özlece <burakozleceplus@gmail.com>
2022-12-26 23:49:59 +0300

    12.49

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using MvcCv.Models.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities1 db = new DbCvEntities1();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkında.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Eğitimlerim()
+        {
+            var eğitimler = db.TblEğitimlerim.ToList();
+            return PartialView(eğitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Seltifikalarım()
+        {
+            var seltifikalar = db.TblSeltifikalarım.ToList();
+            return PartialView(seltifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult İletişim()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult İletişim(Tblİletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tblİletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
